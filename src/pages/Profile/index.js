8d5b44fdab57ce4f8750207a896811117e787bc8
Samuel Santos <32316175+samuxlh@users.noreply.github.com>
2022-12-08 19:17:05 -0300

    fix

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,9 +3,27 @@ import './style.css';
 import ConditionalRender from '../../components/Profile'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { db } from '../../firebase'
+import { collection, getDocs } from "firebase/firestore"
+
 export function ProfilePage() {
 
   const [foundUser, setFoundUser] = useState()
+
+  const [jazigo, setJazigo] = useState([]);
+  const fetchPost = async () => {
+    await getDocs(collection(db, "jazigo"))
+      .then((querySnapshot) => {
+        const newData = querySnapshot.docs
+          .map((doc) => ({ ...doc.data(), id: doc.id }));
+        setJazigo(newData);
+        window.localStorage.setItem('dbUsers', JSON.stringify(newData));
+        console.log(jazigo, newData);
+      })
+  }
+  useEffect(() => {
+    fetchPost();
+  }, [])
 
   useEffect(() => {
     const currentUser = JSON.parse(window.localStorage.getItem('currentUser'))
